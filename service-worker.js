82e15b11faf476ffc9cbf435d314b8b238c8e7f9
kulkankaraeski  <kulkankaraeski@gmail.com>
2026-04-16 12:15:15 +0300

    service-worker silindi

--- service-worker.js
+++ service-worker.js
@@ -1,71 +1,17 @@
-const CACHE_NAME = 'dokumhane-v2';
-const DATA_CACHE_NAME = 'dokumhane-data-v2';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
+// Service Worker'ı ve önbellekleri tamamen temizlemek için "Kill Switch" (İptal Anahtarı)
 
-// Kurulum Aşaması: Statik dosyaları önbelleğe al
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS_TO_CACHE))
-  );
+  // Yeni worker'ın hemen devreye girmesini ve eskisinin yerini almasını sağla
   self.skipWaiting();
 });
 
-// Aktivasyon: Eski önbellekleri temizle
 self.addEventListener('activate', event => {
   event.waitUntil(
+    // Tüm mevcut önbellekleri (caches) sil ve Service Worker kaydını tamamen iptal et (unregister)
     caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME && name !== DATA_CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
+      return Promise.all(cacheNames.map(name => caches.delete(name)));
+    }).then(() => {
+      return self.registration.unregister();
     })
-  );
-  // Service Worker güncellendiğinde uygulamanın beklemeyip hemen kontrolü almasını sağlar
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  // POST istekleri (form gönderme gibi) önbelleğe alınmamalıdır.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-
-  const url = new URL(event.request.url);
-
-  // Veri API'leri için (opensheet.elk.sh), önce ağa git, başarısız olursa önbelleği kullan.
-  // Bu, çevrimdışıyken bile uygulamanın son çekilen veriyi göstermesini sağlar.
-  if (url.hostname === 'opensheet.elk.sh') {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(event.request).then(networkResponse => {
-          if (networkResponse && networkResponse.status === 200) {
-            cache.put(event.request, networkResponse.clone());
-          }
-          return networkResponse;
-        }).catch(() => {
-          // Ağ isteği başarısız oldu, önbellekten sunmayı dene.
-          return cache.match(event.request).then(cachedResponse => {
-            if (cachedResponse) {
-              return cachedResponse;
-            }
-            // Önbellekte de veri yoksa hata fırlat (index.html'deki hata mesajını tetiklemek için)
-            throw new Error('İnternet bağlantısı yok ve önbellek boş.');
-          });
-        });
-      })
-    );
-    return;
-  }
-
-  // Diğer tüm GET istekleri (uygulama kabuğu) için önbelleği öncelikli kullan.
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
